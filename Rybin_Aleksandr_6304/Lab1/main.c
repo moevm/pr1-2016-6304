73fdd21c7cc4ccbbe8d0ad5ce0ea262317925fea
AleksandrRybin <vfibyrf007@gmail.com>
2016-10-10 21:31:07 +0300

    Lab1

--- Rybin_Aleksandr_6304/Lab1/main.c
+++ Rybin_Aleksandr_6304/Lab1/main.c
@@ -1,8 +1,5 @@
 #include <string.h>
-<<<<<<< HEAD
-#include "print_str.h"
-#include "get_name.h"	
-=======
+#include <stdio.h>
 #include "get_name.h"
 #include "print_str.h"
 >>>>>>> de1d2125e6834bbc48a4f12a753a527654a21707
