b9836bfea35c43f682fe1ba9a0e8106156e60958
almostpurplerain <zapevalov97@mail.ru>
2017-06-02 16:56:18 +0300

    Update main.c

--- Alexey_Zapevalov_l_6/main.c
+++ Alexey_Zapevalov_l_6/main.c
@@ -2,16 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-int push(int* array, int j, char* a) //adds new element
+typedef struct Stack{
+    int mas[200];
+    int size;
+} Stack;
+
+void push(Stack *stack, char* a) //adds new element
 {
-    array[j]=atoi(a);
+    stack->mas[stack->size]=atoi(a);
     printf("ok\n");
-    return j+1;
+    stack->size++;
 }
 
-void size(int j) //shows current index
+void size(Stack *stack) //shows current size
 {
-    printf("%d\n", j);
+    printf("%d\n", stack->size);
 }
 
 int printexit()//ends program
@@ -20,24 +25,24 @@ int printexit()//ends program
     return 1;
 }
 
-int pop(int* array, int j)//prints and deletes top element
+void pop(Stack *stack)//prints and deletes top element
 {
-    printf("%d\n", array[j-1]);
-    return j-1;
+    printf("%d\n", stack->mas[stack->size-1]);
+    stack->size--;
 }
 
-void top(int* array, int j)//shows top element
+void top(Stack *stack)//shows top element
 {
-    printf("%d\n", array[j]);
+    printf("%d\n", stack->mas[stack->size-1]);
 }
 
 int main()
 {
+    Stack stack;
     int i=0, c=1, n, j=0, flag=0;   // c - input strings counter, n - result strings amount counter
     char del[]="\n ";               // i - strtok counter, j - result counter
     char vvod[200][20];             // del - dividing symbols array, vvod - input array
-    char mas[200][20];              // mas - in-between result counter
-    int array[200];                 // array - result counter
+    char mas[200][20];              // mas - in-between result counter                // array - result counter
     vvod[0][0]='0';                 //checking previous result needs array to begin with 1
     while(strcmp(vvod[c-1], "exit\n")!=0)   //reading strings
     {
@@ -61,26 +66,26 @@ int main()
         {
             if (strcmp(mas[n], "push")==0)
             {
-                j=push(array, j, mas[n+1]);
+                push(&stack, mas[n+1]);
                 n++; //jumps over the number
             }
             else if (strcmp(mas[n], "size")==0)
-            {
-                size(j);
+            {чё
+                size(&stack);
             }
             else if (strcmp(mas[n], "exit")==0)
             {
                 flag=printexit();
             }
-            else if (j!=0)
+            else if (stack.size!=0)
             {
                 if (strcmp(mas[n], "pop")==0)
                 {
-                    j=pop(array, j);
+                    pop(&stack);
                 }
                 else if (strcmp(mas[n], "top")==0)
                 {
-                    top(array, j-1);
+                    top(&stack);
                 }
             }
             else
