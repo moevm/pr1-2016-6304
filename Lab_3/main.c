e2252bef40f2b1d686ce59bca3e08115a6f3831d
ProstoNekitos <Tom_Clancyc@mail.ru>
2017-06-28 15:42:17 +0300

    Update main.c

--- Lab_3/main.c
+++ Lab_3/main.c
@@ -1,100 +1,105 @@
-#define FNUM 200
-#define PLEN 10000
+#define PS 200
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <sys/types.h>
 #include <dirent.h>
 #include <string.h>
+#include <stdlib.h>
 
-char* read(const char*, int*);
-
-int filePath(char *, char [][PLEN], int*, char*);
-
-int main(void){
-    int fnum=0; 
-    char files[200][PLEN];
-    filePath(".", files, &fnum, "file.txt");
-    for (int i=0; i<fnum; i++)
-    {
-        printf("%s", files[i]);
-    }
-    return 0;
-}
-
-int filePath(char *startdir, char files[][PLEN], int* fnum, char* file){
-    char ActivePath[PLEN];
-    strcpy(ActivePath, startdir);
-    DIR *dir; 
-    struct dirent *cat;
-    if (dir = opendir(ActivePath));
-         while(cat = readdir(dir))
-        {     
-            if(cat->d_type==DT_REG && !strcmp(file, cat->d_name) && strstr(cat->d_name,"."))
-            {
-                int path_len = strlen(ActivePath);
-                strcat(ActivePath, "/");
-                strcat(ActivePath, cat->d_name);
-                
-                char* str;
-                int fsize;
-                str = read(ActivePath, &fsize);
-                printf("%s\n", str);
-                
-                if (strstr(str, "Minotaur"))
-                {
-                    printf("FOUND");
-                    strcpy(files[(*fnum)++], ActivePath);
-                    ActivePath[path_len] = '\0';
-                    return 1;
-                }
-                
-                if (strstr(cat->d_name, "Deadlock")) return 0;
-                
-                char token[FNUM-1][fsize];
-                int i = 0;
-                char *tmp=strtok(str," \n");
-                while(tmp)
-                {
-                    strcpy(token[i],tmp);
-                    printf("%s\n", token[i++]);
-                    tmp=strtok(NULL," \n");
-                }
-                for(int j=0; j<i; j++)
-                {
-                    if(strstr(token[i], ".txt"))
-                    {           
-                        filePath(startdir, files, fnum, token[i]);
-                        strcpy(files[(*fnum)++], ActivePath);
-                        ActivePath[path_len] = '\0';
-                    }
-                }
-            }
-        }
-            if(cat->d_type == DT_DIR && strcmp(".",cat->d_name) && strcmp("..", cat->d_name))
-            {
-                int path_len = strlen(ActivePath);
-                strcat(ActivePath,"/");
-                strcat(ActivePath,cat->d_name);
-                filePath(ActivePath, files, fnum, file);
-                ActivePath[path_len] = '\0';
-            }
-    closedir(dir);
-    return 1;
-}
-
-char *read(const char *current_path, int *file_size)
+char *read(char *activepath, int *file_size)
 {
-    FILE *file=fopen(current_path,"rt");
-    
+    FILE *file=fopen(activepath,"rt");
     fseek(file,0, SEEK_END);
     *file_size = ftell(file);
-    fseek(file,0, SEEK_SET);
+    rewind(file);
     
     char *str=(char*)malloc(sizeof(char)*(*file_size));
     int i=0;
-    
     char symb;
     while(fscanf(file,"%c",&symb)>0) str[i++]=symb;
     fclose(file);
     return str;
+}
+
+int list_dir(char *newcur_path,char *startdir, char* file, char **paths)
+{
+    char activepath[10000];
+    int depth=0; 
+    
+    strcpy(activepath,newcur_path); 
+    DIR *dir; 
+    struct  dirent *de; 
+
+    if(dir= opendir(activepath))
+    {
+        while(de = readdir(dir))
+        {
+            int length = strlen(activepath);
+                strcat(activepath,"/");
+                strcat(activepath,de->d_name);
+            if(de->d_type==DT_REG && strstr(file,de->d_name))
+            {
+                int i,j=0,file_size=0;
+                char *str=read(activepath,&file_size);
+                char token[PS-1][file_size];
+                char *buff=strtok(str," \n");
+                while(buff)
+                {
+                    strcpy(token[j],buff);
+                    j++;
+                    buff=strtok(NULL," \n");
+                }
+                for(i=0;i<j;i++)
+                { 
+                    if(!strcmp(token[i],"@include"));
+                        else if(strstr(token[i],"Deadlock")) return depth;
+                        else if(strstr(token[i],"Minotaur"))
+                        {
+                            paths[depth]=(char*)malloc(sizeof(char)*1000);
+                            strcpy(paths[depth],activepath);
+                            depth=1;
+                            return depth;
+                        }
+                        else
+                        {
+                            depth=list_dir(startdir,startdir,token[i],paths);
+                            if(depth!=0)
+                            {
+                                paths[depth]=(char*)malloc(sizeof(char)*1000);
+                                strcpy(paths[depth],activepath);
+                                depth++;
+                                return depth;
+                            }
+                        }
+                }
+            }
+            if( de->d_type == DT_DIR && strcmp(".",de->d_name) && strcmp("..",de->d_name))
+            { 
+                depth=list_dir(activepath,startdir,file,paths);
+            }
+            if (depth!=0)
+            {
+                return depth;
+            }
+            activepath[length] = '\0';
+            de = readdir(dir);
+        }
+        closedir(dir);
+    }
+    return depth;
+}
+
+int main()
+{
+    char **paths=(char**)malloc(PS*sizeof(char*));
+    int depth=0;
+    char *file="file.txt";
+    depth=list_dir(".",".",file,paths);
+    for(int i=depth-1;i>=0;i--)
+    {
+        printf("%s\n",paths[i]);
+        free(paths[i]);
+    }
+    free(paths);
+    return 0;
 }
