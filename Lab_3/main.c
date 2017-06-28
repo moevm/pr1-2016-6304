6f506ef45ef2a27bb04f6b2f4e3e81bb1f6710ed
ProstoNekitos <Tom_Clancyc@mail.ru>
2017-06-28 15:05:00 +0300

    Update main.c

--- Lab_3/main.c
+++ Lab_3/main.c
@@ -1,99 +1,105 @@
-#define FNUM 200
-#define PLEN 10000
+#define PS 200
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <sys/types.h>
 #include <dirent.h>
 #include <string.h>
+#include <stdlib.h>
 
-void fileFinder(char [][PLEN], char *, char*);//Функция поиска файла
-
-int read(char*, char*, char [][PLEN]); //Функция считывания файлов
-
-void filePath(char *, char [][PLEN], int*); //Функция записи пути
-
-int main(void){
-    int fnum=0; 
-    char files[200][PLEN];
-    filePath(".", files, &files_num);
-    fileFinder(files, "file.txt", "");
-    return 0;
+char *read(char *activepath, int *file_size)
+{
+    FILE *file=fopen(activepath,"rt");
+    fseek(file,0, SEEK_END);
+    *file_size = ftell(file);
+    rewind(file);
+    
+    char *str=(char*)malloc(sizeof(char)*(*file_size));
+    int i=0;
+    char symb;
+    while(fscanf(file,"%c",&symb)>0) str[i++]=symb;
+    fclose(file);
+    return str;
 }
 
-void fileFinder(char files[][PLEN], char *currFile, char* empty){
-    char buffer[PLEN];
-    char *ptext;
-    for(int i=0;i<FNUM;i++)
+int list_dir(char *newcur_path,char *startdir, char* file, char **paths)
+{
+    char activepath[10000];
+    int depth=0; 
+    
+    strcpy(activepath,newcur_path); 
+    DIR *dir; 
+    struct  dirent *de; 
+
+    if(dir= opendir(activepath))
     {
-        if(strstr(files[i], currFile)!=NULL)
+        while(de = readdir(dir))
         {
-            strcpy(buffer, empty);
-            strcat(buffer, files[i]);
-            strcat(buffer, "\n");
-            if(read(files[i], buffer, files))
-            { 
-                ptext=strtok(buffer, "\n");
-                while(ptext!=NULL)
+            int length = strlen(activepath);
+                strcat(activepath,"/");
+                strcat(activepath,de->d_name);
+            if(de->d_type==DT_REG && strstr(de->d_name,file))
+            {
+                int i,j=0,file_size=0;
+                char *str=read(activepath,&file_size);
+                char token[PS-1][file_size];
+                char *buff=strtok(str," \n");
+                while(buff)
                 {
-                    printf("%s\n",(ptext+2));
-                    ptext=strtok(NULL, "\n");
+                    strcpy(token[j],buff);
+                    j++;
+                    buff=strtok(NULL," \n");
+                }
+                for(i=0;i<j;i++)
+                { 
+                    if(!strcmp(token[i],"@include"));
+                        else if(strstr(token[i],"Deadlock")) return depth;
+                        else if(strstr(token[i],"Minotaur"))
+                        {
+                            paths[depth]=(char*)malloc(sizeof(char)*1000);
+                            strcpy(paths[depth],activepath);
+                            depth=1;
+                            return depth;
+                        }
+                        else
+                        {
+                            depth=list_dir(startdir,startdir,token[i],paths);
+                            if(depth!=0)
+                            {
+                                paths[depth]=(char*)malloc(sizeof(char)*1000);
+                                strcpy(paths[depth],activepath);
+                                depth++;
+                                return depth;
+                            }
+                        }
                 }
             }
+            if( de->d_type == DT_DIR && strcmp(".",de->d_name) && strcmp("..",de->d_name))
+            { 
+                depth=list_dir(activepath,startdir,file,paths);
+            }
+            if (depth!=0)
+            {
+                return depth;
+            }
+            activepath[length] = '\0';
+            de = readdir(dir);
         }
+        closedir(dir);
     }
+    return depth;
 }
 
-void filePath(char *startdir, char files[][PLEN], int* files_num){
-    char ActivePath[PLEN]; //Массив под путь
-    strcpy(ActivePath, startdir);
-    DIR *dir; 
-    struct dirent *cat = readdir(dir);
-    if(dir = opendir(ActivePath));
-        while(cat = readdir(dir))//Пока есть что читать
-        {
-            if(cat->d_type==DT_REG)
-            {
-                int path_len = strlen(ActivePath);
-                strcat(ActivePath,"/");
-                strcat(ActivePath,cat->d_name);
-                strcpy(files[(*files_num)++], ActivePath);
-                ActivePath[path_len] = '\0'; //Добавляем в конец \0, чтобы strcat работал
-            }
-            if(cat->d_type == DT_DIR && strcmp(".",cat->d_name) && strcmp("..", cat->d_name))
-            {
-                int path_len = strlen(ActivePath); //Изменяем длину пути на текущий
-                strcat(ActivePath,"/");
-                strcat(ActivePath,cat->d_name);
-                filePath(ActivePath, files, files_num);
-                ActivePath[path_len] = '\0';
-            }
-        }
-  closedir(dir);
-}
-
-int read(char* filename, char* empty, char files[][PLEN]){
-    FILE *file; 
-    if ((file = fopen (filename, "r")) != NULL)//Открываем файл на чтение и проверяем на правильность
-    { 
-        char str [4000]; //Буфер для содержимого
-        while (fgets(str, sizeof(str), file)!= NULL) //Считываем файл
-        {           
-            if (!strcmp(str, "Minotaur"))
-                return 1;
-	    char *ptext;//Объвляем массив для внутренних инклюдов
-	    ptext=strtok(str, " \n\r"); 
-	    while(ptext!=NULL)
-            {
-                if(strstr(ptext, ".txt"))
-                    fileFinder(files, ptext, empty); //Начинаем поиск файлов
-                ptext=strtok(NULL, " \n\r"); 
-	    }
-        }
-        fclose(file);
-    } 
-    else
+int main()
+{
+    char **paths=(char**)malloc(PS*sizeof(char*));
+    int depth=0;
+    char *file="file.txt";
+    depth=list_dir(".",".",file,paths);
+    for(int i=depth-1;i>=0;i--)
     {
-        perror(filename);
+        printf("%s\n",paths[i]);
+        free(paths[i]);
     }
-     return 0;
+    free(paths);
+    return 0;
 }
